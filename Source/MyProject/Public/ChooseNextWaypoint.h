1b0af9c9001d16eeca6e09448a37dccba4317b99
DESKTOP-KEOIA4P\Christo Gladiatore <christian.dellaccio@gmail.com>
2018-11-06 11:08:26 +0100

    cancellazione patrolling guard classes da unreal

--- Source/MyProject/Public/ChooseNextWaypoint.h
+++ Source/MyProject/Public/ChooseNextWaypoint.h
@@ -16,5 +16,10 @@ class MYPROJECT_API UAITasks : public UBTTaskNode
 	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
-	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
 };
